f1c37e03f3f177e3bf359ec73254146078ae1718
chris@chris <chris@chris>
2018-03-23 10:24:16 +0100

    reset de 5 commit

---
+++ var/cache/dev/twig/57/57d8e784adc1f9dd26b379f4c7f79320086f760b676a6352c4169f9dc2030672.php
@@ -0,0 +1,266 @@
+<?php
+
+/* tra_travail/index.html.twig */
+class __TwigTemplate_84401b68ccd30bb4e028aedd947fba0af1eee80ccc92e1e97e6a765297509691 extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        // line 1
+        $this->parent = $this->loadTemplate("base.html.twig", "tra_travail/index.html.twig", 1);
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "tra_travail/index.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "tra_travail/index.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 4
+        echo "    
+<div class=\"container\">
+    <h2>Liste des Chantier</h2>
+        <div class=\"row\">
+            <table class=\"table table-bordered\" id=\"liste_chantier\">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Titre</th>
+                        <th>Description</th>
+                        <th>Prix</th>
+                        <th>Date debut</th>
+                        <th>Date rappel</th>
+                        <th>Mode paiment</th>
+                        <th>Lieux</th>
+                        <th>Garantie</th>
+                        <th>Devis</th>
+                        <th>Process verbal</th>
+                        <th>Facture</th>
+                        
+                    </tr>
+                </thead>
+        </div>        
+                
+                <div class=\"row\">
+                    <tbody>
+                ";
+        // line 30
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["tRA_travails"]) || array_key_exists("tRA_travails", $context) ? $context["tRA_travails"] : (function () { throw new Twig_Error_Runtime('Variable "tRA_travails" does not exist.', 30, $this->source); })()));
+        foreach ($context['_seq'] as $context["_key"] => $context["tRA_travail"]) {
+            // line 31
+            echo "                        <tr>
+                            <td><a href=\"";
+            // line 32
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 33
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "titre", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 34
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "description", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 35
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "prix", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 36
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            if (twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "dateDebut", array())) {
+                echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "dateDebut", array()), "Y-m-d"), "html", null, true);
+            }
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 37
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            if (twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "dateRappel", array())) {
+                echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "dateRappel", array()), "Y-m-d"), "html", null, true);
+            }
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 38
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "modePaiment", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 39
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "lieux", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 40
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "garantie", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 41
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "devis", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 42
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "processVerbal", array()), "html", null, true);
+            echo "</a></td>
+                            <td><a href=\"";
+            // line 43
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_show", array("id" => twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["tRA_travail"], "facture", array()), "html", null, true);
+            echo "</a></td>
+
+
+                            ";
+            // line 57
+            echo "                ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['tRA_travail'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 58
+        echo "                        </tbody>
+                    </table>
+                </div>
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "tra_travail/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  172 => 58,  166 => 57,  158 => 43,  152 => 42,  146 => 41,  140 => 40,  134 => 39,  128 => 38,  120 => 37,  112 => 36,  106 => 35,  100 => 34,  94 => 33,  88 => 32,  85 => 31,  81 => 30,  53 => 4,  44 => 3,  15 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'base.html.twig' %}
+
+{% block body %}
+    
+<div class=\"container\">
+    <h2>Liste des Chantier</h2>
+        <div class=\"row\">
+            <table class=\"table table-bordered\" id=\"liste_chantier\">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Titre</th>
+                        <th>Description</th>
+                        <th>Prix</th>
+                        <th>Date debut</th>
+                        <th>Date rappel</th>
+                        <th>Mode paiment</th>
+                        <th>Lieux</th>
+                        <th>Garantie</th>
+                        <th>Devis</th>
+                        <th>Process verbal</th>
+                        <th>Facture</th>
+                        
+                    </tr>
+                </thead>
+        </div>        
+                
+                <div class=\"row\">
+                    <tbody>
+                {% for tRA_travail in tRA_travails %}
+                        <tr>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.id }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.titre }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.description }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.prix }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{% if tRA_travail.dateDebut %}{{ tRA_travail.dateDebut|date('Y-m-d') }}{% endif %}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{% if tRA_travail.dateRappel %}{{ tRA_travail.dateRappel|date('Y-m-d') }}{% endif %}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.modePaiment }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.lieux }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.garantie }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.devis }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.processVerbal }}</a></td>
+                            <td><a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">{{ tRA_travail.facture }}</a></td>
+
+
+                            {# <td>
+                                <ul>
+                                    <li>
+                                        <a href=\"{{ path('fiche_chantier_show', { 'id': tRA_travail.id }) }}\">show</a>
+                                    </li>
+                                    <li>
+                                        <a href=\"{{ path('fiche_chantier_edit', { 'id': tRA_travail.id }) }}\">Modifier </a>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr> #}
+                {% endfor %}
+                        </tbody>
+                    </table>
+                </div>
+
+{% endblock %}
+", "tra_travail/index.html.twig", "/home/chris/symfony-projet/SPH2/app/Resources/views/tra_travail/index.html.twig");
+    }
+}
---
+++ var/cache/dev/twig/6d/6de3df77d4f7c3116f6571511f4054e903b4ecd4736aca0a639c5bd6ff14a3b0.php
@@ -0,0 +1,193 @@
+<?php
+
+/* navBar.html.twig */
+class __TwigTemplate_4d72311cce2ed40986c853291480f6c52475d28205259b5a17d1d1ddb48bc7f3 extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "navBar.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "navBar.html.twig"));
+
+        // line 1
+        echo "
+<section class=\"container\">
+    <nav class=\"navbar navbar-default navbar-static-top\" role=\"navigation\">
+        <!-- logo renvoyant vers la liste des chantiers -->
+            <div class=\"col-lg-1  nav navbar-nav\">
+                <a href=\"";
+        // line 6
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_chantier_index");
+        echo "\">
+                    <img class=\"padding\" src=\"";
+        // line 7
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("sph.ico"), "html", null, true);
+        echo "\" />
+                </a>
+            </div>
+        
+
+                <!-- bouton pour navbar responsive   -->
+                <div class=\"navbar-header\">
+                    <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">
+                        <span class=\"sr-only\">Toggle navigation</span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                    </button>
+                </div>
+        </ul>
+
+        <!-- contenu de la navbar -->
+        <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+            <ul class=\"nav navbar-nav\">
+                <li class=\"hover\"><a href=\"";
+        // line 26
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fiche_client_new");
+        echo "\">Ajouter un client</a></li>
+                <li>
+        <!-- formulaire de recherche pour large device -->
+                <form class=\"navbar-form navbar-default col-lg-3\"  method=\"POST\" action=\"?p=recherche_client\">
+                        <input type=\"search\" class=\"form-control hidden-xs\" size=\"30\" placeholder=\"Rechercher un client par son nom\" aria-controls=\"liste_client\" id=\"recherche\" name=\"recherche\"/>
+                        <button type=\"submit\" class=\"btn btn-success hidden-xs\"><i class=\"glyphicon glyphicon-search\"></i></button>
+                </form>
+        <!-- formulaire de recherche pour small device -->
+                <form class=\"navbar-form navbar-right\"  method=\"POST\" action=\"?p=recherche_client\">
+                    <div class=\"visible-xs col-xs-8\">
+                        <input type=\"text\" class=\"form-control\" placeholder=\"Rechercher un client par son nom\"  id=\"recherche\" name=\"recherche\"/>
+                    </div>
+                    <button type=\"submit\" class=\"btn btn-success visible-xs\"><i class=\"glyphicon glyphicon-search\"></i></button>
+                </form>
+                </li>
+            </ul>
+            
+                <form class=\"nav navbar-nav navbar-right list-inline\">
+                <div class=\"dropdown, right\">
+                    <button class=\"btn btn-success dropdown-toggle\" href=\"#\" role=\"button\" id=\"dropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                        Parametre
+                    <span class=\"caret\"></span>
+                    </button>
+                    <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuLink\">
+                        <li><a id=\"hoverDeco\" href=\"";
+        // line 50
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("fos_user_security_logout");
+        echo "\">Se déconnecter</a></li>
+                    </ul>
+                </div>
+                </form>
+
+
+                
+            </ul> 
+            
+
+        <!-- fin du navbar-collapse -->
+
+    </nav>
+</section>";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "navBar.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  89 => 50,  62 => 26,  40 => 7,  36 => 6,  29 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("
+<section class=\"container\">
+    <nav class=\"navbar navbar-default navbar-static-top\" role=\"navigation\">
+        <!-- logo renvoyant vers la liste des chantiers -->
+            <div class=\"col-lg-1  nav navbar-nav\">
+                <a href=\"{{ path('fiche_chantier_index') }}\">
+                    <img class=\"padding\" src=\"{{ asset('sph.ico') }}\" />
+                </a>
+            </div>
+        
+
+                <!-- bouton pour navbar responsive   -->
+                <div class=\"navbar-header\">
+                    <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">
+                        <span class=\"sr-only\">Toggle navigation</span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                    </button>
+                </div>
+        </ul>
+
+        <!-- contenu de la navbar -->
+        <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+            <ul class=\"nav navbar-nav\">
+                <li class=\"hover\"><a href=\"{{ path('fiche_client_new') }}\">Ajouter un client</a></li>
+                <li>
+        <!-- formulaire de recherche pour large device -->
+                <form class=\"navbar-form navbar-default col-lg-3\"  method=\"POST\" action=\"?p=recherche_client\">
+                        <input type=\"search\" class=\"form-control hidden-xs\" size=\"30\" placeholder=\"Rechercher un client par son nom\" aria-controls=\"liste_client\" id=\"recherche\" name=\"recherche\"/>
+                        <button type=\"submit\" class=\"btn btn-success hidden-xs\"><i class=\"glyphicon glyphicon-search\"></i></button>
+                </form>
+        <!-- formulaire de recherche pour small device -->
+                <form class=\"navbar-form navbar-right\"  method=\"POST\" action=\"?p=recherche_client\">
+                    <div class=\"visible-xs col-xs-8\">
+                        <input type=\"text\" class=\"form-control\" placeholder=\"Rechercher un client par son nom\"  id=\"recherche\" name=\"recherche\"/>
+                    </div>
+                    <button type=\"submit\" class=\"btn btn-success visible-xs\"><i class=\"glyphicon glyphicon-search\"></i></button>
+                </form>
+                </li>
+            </ul>
+            
+                <form class=\"nav navbar-nav navbar-right list-inline\">
+                <div class=\"dropdown, right\">
+                    <button class=\"btn btn-success dropdown-toggle\" href=\"#\" role=\"button\" id=\"dropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                        Parametre
+                    <span class=\"caret\"></span>
+                    </button>
+                    <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuLink\">
+                        <li><a id=\"hoverDeco\" href=\"{{ path('fos_user_security_logout') }}\">Se déconnecter</a></li>
+                    </ul>
+                </div>
+                </form>
+
+
+                
+            </ul> 
+            
+
+        <!-- fin du navbar-collapse -->
+
+    </nav>
+</section>", "navBar.html.twig", "/home/chris/symfony-projet/SPH2/app/Resources/views/navBar.html.twig");
+    }
+}
